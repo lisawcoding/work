cd404799d0be80712728c0e8c66c83981898bcef
lisacoding <tolisacoding@gmail.com>
2019-01-02 10:31:07 +0800

    Update bbc.js

--- bbc.js
+++ bbc.js
@@ -1,6 +1,7 @@
     var header=document.querySelector("header");
     var main=document.querySelector("main");  
-    var h=window.getComputedStyle(header,null).getPropertyValue("height");      
+    
+    var h=window.getComputedStyle(header,null).getPropertyValue("height");
     window.addEventListener("load", function(){
         var h=window.getComputedStyle(header,null).getPropertyValue("height");   
         main.style.marginTop=h;
@@ -9,14 +10,20 @@
         var h=window.getComputedStyle(header,null).getPropertyValue("height");   
         main.style.marginTop=h;
     })    
-    
-    
+
     document.querySelector(".hamburger").addEventListener("click", function(){
-            document.querySelector(".navItem-container").classList.toggle("show");
-            var h=window.getComputedStyle(header,null).getPropertyValue("height");   
+            document.querySelector(".navItem-container").classList.toggle("flex");
+
+            var h=window.getComputedStyle(header,null).getPropertyValue("height");
+            var bars=document.querySelector(".hamburger .fa-bars");
+            var times=document.querySelector(".hamburger .fa-times");
             main.style.marginTop=h;
+            
+            bars.classList.toggle("none");
+            if(window.getComputedStyle(bars).display=="none"){
+               times.style.display="block" 
+            } else {
+                times.style.display="none";
+            }
         })
-        
-        
-        
         
